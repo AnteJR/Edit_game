a51676db7ad41168104be42807f17c8193c4ed62
Joël Rimaz <jo.ante2011@gmail.com>
2019-11-22 17:32:58 +0100

    Most of the work done

--- labo-reecriture/index.js
+++ labo-reecriture/index.js
@@ -1,21 +1,24 @@
-var backgroundImages;
-var monde;
-var flash_power;
+var backgroundImages; 
+var monde; 
+var balle; 
+var flash_power; 
+var versdroite = true; 
+var inCombo = false; 
 var Breakout = new Phaser.Class({
 
     Extends: Phaser.Scene,
-
+    
     initialize:
-
+    
     function Breakout ()
     {
         Phaser.Scene.call(this, { key: 'breakout' });
-
+    
         this.bricks;
         this.paddle;
         this.ball;
     },
-
+    
     preload: function ()
     {
         this.load.atlas('assets', 'breakout/breakout.png', 'breakout/breakout.json', 'breakout/meadow.jpg');
@@ -29,63 +32,69 @@ var Breakout = new Phaser.Class({
         this.load.image('powerup', 'breakout/powerup.png');
         this.load.image('meadow', 'breakout/meadow.jpg')
     },
-
+    
     create: function ()
     {
         backgroundImages = this.add.image(400, 300, 'meadow');
         backgroundImages.width = 800;
         monde = this.physics.world;
+        balle = this.physics.ball;
         scoreText = this.add.text(20, 20, 'Score : 0', { fontFamily: '"Roboto Condensed"' });
         comboText = this.add.text(20, 40, 'Combo x0', { fontFamily: '"Roboto Condensed"' });
         lifeText = this.add.text(750, 20, 'Life x5', { fontFamily: '"Roboto Condensed"' });
-
+    
         flash_power = this.add.image(400, 300, 'powerup');
         flash_power.alpha = 0;
-        
+    
         this.music = this.sound.add('ost',{loop: true})
         this.music.play()
-        
+    
         //  Enable world bounds, but disable the floor
         monde.setBoundsCollision(true, true, true, false);
-
+    
         //  Create the bricks in a 10x6 grid
-        this.bricks = this.physics.add.staticGroup({
+        this.bricks = this.physics.add.group({
             key: 'assets', frame: [ 'blue1', 'red1', 'green1', 'yellow1', 'silver1', 'purple1' ],
             frameQuantity: 10,
             gridAlign: { width: 10, height: 6, cellWidth: 64, cellHeight: 32, x: 112, y: 100 },
             timedEvent: this.time.addEvent({ delay: 500, callback: this.onEvent, callbackScope: this, loop: true })
         });
-
+    
+        this.bricks.children.each(e => {
+            e.setImmovable();
+            e.vers_droite = true;
+        });
+    
         this.ball = this.physics.add.image(400, 500, 'assets', 'ball1').setCollideWorldBounds(true).setBounce(1);
         this.ball.setData('onPaddle', true);
-
+    
         this.paddle = this.physics.add.image(400, 550, 'assets', 'paddle1').setImmovable();
-
+    
         //  Our colliders
         this.physics.add.collider(this.ball, this.bricks, this.hitBrick, null, this);
         this.physics.add.collider(this.ball, this.paddle, this.hitPaddle, null, this);
-
+    
         //  Input events
         this.input.on('pointermove', function (pointer) {
-
+    
             //  Keep the paddle within the game
             this.paddle.x = Phaser.Math.Clamp(pointer.x, 52, 748);
-
+    
             if (this.ball.getData('onPaddle'))
             {
                 this.ball.x = this.paddle.x;
             }
-
+    
         }, this);
-
+    
         this.input.on('pointerup', function (pointer) {
-
+    
             if (this.ball.getData('onPaddle'))
             {
                 this.ball.setVelocity(-75, -300);
                 this.ball.setData('onPaddle', false);
             }
-
+    
         }, this);
     },
     onEvent: function ()
@@ -94,25 +103,27 @@ var Breakout = new Phaser.Class({
         //this.bricks = this.physics.setVelocity(0,100);
         //this.bricks = this.physics.moveTo(112, 64);
     },
-
+    
     hitBrick: function (ball, brick)
     {
         brick.disableBody(true, true);
         // son mouton capturé
         this.sound.play('beeh',{volume: 0.25});
-
+    
         score+= 100+(100*multiplier);
         multiplier = freeBounce*0.05;
         freeBounce++;
         comboText.setText("Combo x"+freeBounce);
         console.log(score + " " + freeBounce + " " + multiplier);
         scoreText.setText("Score : "+score);
-        if(freeBounce == 15)
+        if(freeBounce == 10)
         {
             this.sound.play('combo');
-            this.combo();
+            if(!inCombo){
+                this.combo();
+            }
         }
-
+    
         if (this.bricks.countActive() === 0)
         {
             //insérer un écran de victoire
@@ -124,18 +135,23 @@ var Breakout = new Phaser.Class({
             this.scene.start('victoryscreen');
         }
     },
-
+    
     combo: function ()
     {
         //superpower
         monde.setBoundsCollision(true);
         flash_power.alpha = 0.5;
+        this.ball.body.velocity.x*=2;
+        this.ball.body.velocity.y*=2;        
         setTimeout(function(){ 
             monde.setBoundsCollision(true, true, true, false);
             flash_power.alpha = 0;
+            balle.body.velocity.x = 1;
+            balle.body.velocity.y = 1;
+            inCombo = false;
         }, 5000);
     },
-
+    
     resetBall: function ()
     {   
         this.ball.setVelocity(0);
@@ -146,7 +162,7 @@ var Breakout = new Phaser.Class({
         //son perte balle
         this.sound.play('lost');
     },
-
+    
     resetLevel: function ()
     {
         this.resetBall();
@@ -155,14 +171,14 @@ var Breakout = new Phaser.Class({
         scoreText.setText("Score : "+score);
         ballLeft = 5;
         lifeText.setText("Life x"+ballLeft);
-
+    
         this.bricks.children.each(function (brick) {
-
+    
             brick.enableBody(false, 0, 0, true, true);
-
+    
         });
     },
-
+    
     hitPaddle: function (ball, paddle)
     {
         var diff = 0;
@@ -170,7 +186,7 @@ var Breakout = new Phaser.Class({
         comboText.setText("Combo x"+freeBounce);
         // son rebond
         this.sound.play('bounce');
-
+    
         if (ball.x < paddle.x)
         {
             //  Ball is on the left-hand side of the paddle
@@ -190,9 +206,28 @@ var Breakout = new Phaser.Class({
             ball.setVelocityX(2 + Math.random() * 8);
         }
     },
-
+    
     update: function ()
     {
+        this.bricks.children.each(mouton => {
+            if(mouton.x>config.width-16 && mouton.vers_droite){
+                mouton.vers_droite = false;
+                mouton.y += 16;
+            }
+            if(mouton.x<16 && !mouton.vers_droite){
+                mouton.vers_droite = true;
+                mouton.y += 16;
+            }
+            if(mouton.vers_droite){
+                mouton.x +=1;
+            }
+            else{
+                mouton.x -=1;
+            }
+    
+        })
+    
+    
         if (this.ball.y > 600)
         {
             ballLeft--;
@@ -212,21 +247,20 @@ var Breakout = new Phaser.Class({
             this.resetBall();
         }
     }
-
-});
-
-var GameOver = new Phaser.Class({
-
+    });
+    
+    var GameOver = new Phaser.Class({
+    
     Extends: Phaser.Scene,
-
+    
     initialize:
-
+    
     function GameOver ()
     {
         Phaser.Scene.call(this, { key: 'gameover'});
         window.OVER = this;
     },
-
+    
     create: function()
     {
         this.add.text(100, 200, 'GAME OVER', { fontSize: 100, fontFamily: '"Roboto Condensed"' });
@@ -235,21 +269,20 @@ var GameOver = new Phaser.Class({
             this.scene.start('breakout');
         },this);
     }
-
-});
-
-var VictoryScreen = new Phaser.Class({
-
+    });
+    
+    var VictoryScreen = new Phaser.Class({
+    
     Extends: Phaser.Scene,
-
+    
     initialize:
-
+    
     function VictoryScreen ()
     {
         Phaser.Scene.call(this, { key: 'victoryscreen' });
         window.OVER = this;
     },
-
+    
     create: function()
     {
         this.add.text(100, 200, 'YOU WON!!!', { fontSize: 100, fontFamily: '"Roboto Condensed"' });
@@ -259,25 +292,28 @@ var VictoryScreen = new Phaser.Class({
             this.scene.start('breakout');
         },this);
     }
-});
-
-var config = {
-    type: Phaser.WEBGL,
-    width: 800,
-    height: 600,
-    parent: 'phaser-example',
-    scene: [ Breakout, GameOver, VictoryScreen ],
-    physics: {
-        default: 'arcade'
-    },
-};
-
-var ballLeft = 5;
-var score = 0;
-var multiplier = 0;
-var freeBounce = 0;
-var scoreText;
-var comboText;
-var lifeText;
-
-var game = new Phaser.Game(config);
+    });
+    
+    var config = { 
+        type: Phaser.WEBGL, 
+        width: 800, 
+        height: 600, 
+        parent: 'phaser-example', 
+        scene: [ Breakout, GameOver, VictoryScreen ], 
+        physics: { 
+            default: 'arcade', 
+            arcade : { 
+                debug : true 
+            } 
+        }, 
+    };
+    
+    var ballLeft = 5; 
+    var score = 0; 
+    var multiplier = 0; 
+    var freeBounce = 0; 
+    var scoreText; 
+    var comboText; 
+    var lifeText;
+    
+    var game = new Phaser.Game(config);
